7305ff6d964dd4f3fad3f8649d22d427a0118a9e
root <root@ip-172-31-5-165.ec2.internal>
2024-11-16 13:44:13 +0000

    commit

---
+++ alb.tf
@@ -0,0 +1,40 @@
+resource "aws_lb" "external-alb" {
+name="Externa-LB"
+internal=false
+load_balancer_type="application"
+security_groups=[aws_security_group.demosg.id]
+subnets=[aws_subnet.public_subnet-1.id,aws_subnet.public_subnet-1.id]
+}
+
+
+resource "aws_lb_target_group" "external-alb" {
+name="ALB-TG"
+port=80
+protocol="HTTP"
+vpc_id=aws_vpc.demovpc.id
+}
+
+resource "aws_lb_target_group_attachment" "attachment1" {
+target_group_arn=aws_lb_target_group.external-alb.arn
+target_id="${aws_instance.demoinstance.id}"
+port=80
+depends_on=[aws_instance.demoinstance]
+}
+
+resource "aws_lb_target_group_attachment" "attachment2" {
+target_group_arn=aws_lb_target_group.external-alb.arn
+target_id="${aws_instance.demoinstance.id}"
+port=80
+depends_on=[
+aws_instance.demoinstance1]
+}
+
+resource "aws_lb_listener" "external-alb" {
+load_balancer_arn=aws_lb.external-alb.arn
+port=80
+protocol="HTTP"
+default_action{
+type="forward"
+target_group_arn=aws_lb_target_group.external-alb.arn
+}
+}
